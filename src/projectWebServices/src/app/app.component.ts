cfe3fe65e1d625e7c81275a5058c2509bf99d1af
kingbite <harald.de.vriendt@hotmail.com>
2017-11-28 23:16:22 +0100

    Overzicht + nieuwe componenten
    login, edit report, overzicht aangemaakt.
    
    Overzicht geeft met ngFor static voorbeelddata weer.
    Date picker toegevoegd aan menu.

--- src/projectWebServices/src/app/app.component.ts
+++ src/projectWebServices/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  selectedDate = "28/11"
+  instructorName = 'Naam Instructeur';
 }
--- src/projectWebServices/src/app/app.module.ts
+++ src/projectWebServices/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { AppComponent } from './app.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
+import { OverzichtComponent } from './overzicht/overzicht.component';
+import { EditReportComponent } from './edit-report/edit-report.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePickerComponent,
+    OverzichtComponent,
+    EditReportComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
