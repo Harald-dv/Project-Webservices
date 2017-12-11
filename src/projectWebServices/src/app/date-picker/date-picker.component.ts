16a2973c4eff6457dc59fa33ec2011bb72de62d7
kingbite <harald.de.vriendt@hotmail.com>
2017-12-11 18:21:42 +0100

    Datepicker basic functionallity
    Basic functionallity added to datepicker. Shown data is hardcoded. db
    connectivity to be added.

--- src/projectWebServices/src/app/date-picker/date-picker.component.ts
+++ src/projectWebServices/src/app/date-picker/date-picker.component.ts
@@ -1,27 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+
+import { ReportDbService } from '../report-db.service';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.css']
+  styleUrls: ['./date-picker.component.css'],
+  providers: [ReportDbService]
+
 })
 export class DatePickerComponent implements OnInit {
-  date = 0;
+  date: number;
+  instructor: {id: number, name: string}[] = [];
+  report: {id: number, date: string, instructorId: number, body: string};
+  instructorId=1;
+  show: boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private db: ReportDbService, private http: HttpClient){}
 
   ngOnInit() {
     //this.date = this.route.snapshot.params['date'];
-    this.date = this.route.snapshot.params['date'];
-
-    this.route.params
-    .subscribe(
+    this.route.params.subscribe(
       (params: Params) => {
         this.date = params['date'];
       }
     );
 
-  }
+    this.report = {id: 255, date: this.date.toString(), instructorId: this.instructorId, body: 'dit is de body maar nog zonder inhoud'};
 
+    this.db.getInstructor().subscribe(
+      (data) => {
+        this.instructor= data;
+        //console.log(this.db.instructor);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err);
+      },
+      () => {
+        //console.log("Klaar!");
+        this.show=true;
+      }
+    )
+    this.date = this.route.snapshot.params['date'];
+  }
 }
